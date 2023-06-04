4431b9b2042317ef15edb145ea7883ff0a01ab91
MuriloMoutinho <104277785+MuriloMoutinho@users.noreply.github.com>
2023-06-04 10:59:05 -0300

    converting code from ts

--- app/src/api/weather/get-weather-by-cep.ts
+++ app/src/api/weather/get-weather-by-cep.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "../_base/axios-instance";
 import { API_PATH } from "../../constants";
 
-export async function getWeatherByCep(cep){
+export async function getWeatherByCep(cep: number){
     return await axiosInstance.get(API_PATH.BASE + API_PATH.GET_WEATHER_BY_CEP + cep);
 }
--- app/src/api/weather/get-weather-by-name.ts
+++ app/src/api/weather/get-weather-by-name.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "../_base/axios-instance";
 import { API_PATH } from "../../constants";
 
-export async function getWeatherByName(name){
+export async function getWeatherByName(name: string){
     return await axiosInstance.get(API_PATH.BASE + API_PATH.GET_WEATHER_BY_NAME + name);
 }
